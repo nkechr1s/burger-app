dbd187759194ff750e54b8bee0b995ee7d497904
Nikos Kechris <nkechris22@gmail.com>
2024-07-06 16:56:10 +0300

    Add logout button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,18 @@
+import { useAuth } from "../../context";
 const Header = () => {
-  return <div>Header</div>;
+  const auth = useAuth();
+  return (
+    <div className="">
+      <h1 aria-label="A cool burger app">A cool burger app</h1>
+      {auth.token && (
+        <>
+          <button onClick={() => auth.logout()} className="btn-submit">
+            logout
+          </button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Header;
