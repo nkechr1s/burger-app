d8bc2b90f8765382e2ee212deba2bb03b075f6a3
Nikos Kechris <nkechris22@gmail.com>
2024-07-07 00:57:42 +0300

    Some fixes

--- src/components/Ingredient/Ingredient.tsx
+++ src/components/Ingredient/Ingredient.tsx
@@ -2,11 +2,11 @@ import { Ingredient as IngredientProps } from "../../types";
 import "./Ingredient.css";
 const Ingredient = ({ id, name, src, onClick }: IngredientProps) => {
   return (
-    <li className="ingredient_item" title={name} onClick={onClick}>
+    <li className="ingredient-item" title={name} onClick={onClick}>
       {src && (
-        <div className="ingredient_item__container">
+        <div className="ingredient__item-container">
           <img
-            className="ingredient_item__image"
+            className="ingredient__item-image"
             src={`${process.env.REACT_APP_API_URL}/img/${src}`}
             alt={`${name}-${id}`}
             title={name}
@@ -14,7 +14,7 @@ const Ingredient = ({ id, name, src, onClick }: IngredientProps) => {
           />
         </div>
       )}
-      {name && <div className="ingredient_item__name">{name}</div>}
+      {name && <div className="ingredient__item-name">{name}</div>}
     </li>
   );
 };
