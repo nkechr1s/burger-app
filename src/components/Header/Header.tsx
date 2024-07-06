cebeff45727471cd2059eed09ef380768a4b11b7
Nikos Kechris <nkechris22@gmail.com>
2024-07-06 17:00:29 +0300

    Add container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
 import { useAuth } from "../../context";
+import "./Header.css";
+
 const Header = () => {
   const auth = useAuth();
   return (
-    <div className="">
-      <h1 aria-label="A cool burger app">A cool burger app</h1>
-      {auth.token && (
-        <>
-          <button onClick={() => auth.logout()} className="btn-submit">
-            logout
-          </button>
-        </>
-      )}
+    <div className="container">
+      <header className="header">
+        <h1 aria-label="A cool burger app">A cool burger app</h1>
+        {auth.token && (
+          <>
+            <button onClick={() => auth.logout()} className="btn-submit">
+              logout
+            </button>
+          </>
+        )}
+      </header>
     </div>
   );
 };
