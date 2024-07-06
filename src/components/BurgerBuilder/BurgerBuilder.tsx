7c4a4a6c3c1d47e6cdd41f9e5708cdc6aa0c7f8f
Nikos Kechris <nkechris22@gmail.com>
2024-07-07 01:49:07 +0300

    Remove import react

--- src/components/BurgerBuilder/BurgerBuilder.tsx
+++ src/components/BurgerBuilder/BurgerBuilder.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { fetchIngredients } from "../../lib/api/ingredients";
 import { useAuth } from "../../context";
