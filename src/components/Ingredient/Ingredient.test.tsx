17a453c8ac383c270959a6bb3b6569918725dd4e
Nikos Kechris <nkechris22@gmail.com>
2024-07-07 03:33:14 +0300

    Add more tests on Ingredient

--- src/components/Ingredient/Ingredient.test.tsx
+++ src/components/Ingredient/Ingredient.test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jest/no-mocks-import */
-import { render, screen } from "@testing-library/react";
+import { render, screen,fireEvent } from "@testing-library/react";
 import Ingredient from "./Ingredient";
 import { mockData } from "../../__mocks__/mockIngredient";
 
@@ -15,4 +15,38 @@ test("renders the Ingredient component", () => {
   );
   const ingredient = screen.getByTestId("ingredient");
   expect(ingredient).toBeInTheDocument();
+});
+
+test("calls onClick function when ingredient is clicked", () => {
+  const handleClick = jest.fn(); 
+
+  render(
+    <Ingredient
+      index={0}
+      {...mockData}
+      onClick={handleClick} 
+    />
+  );
+
+  const ingredient = screen.getByTestId("ingredient");
+
+  fireEvent.click(ingredient);
+
+  expect(handleClick).toHaveBeenCalledTimes(1);
+});
+
+test("renders ingredient without image container when src prop is not provided", () => {
+  render(
+    <Ingredient
+      index={0}
+      name={mockData.name}
+      id={mockData.id}
+      src=""
+      onClick={() => {}} 
+    />
+  );
+
+  const ingredientItemContainer = screen.queryByTestId("ingredient-item-container");
+
+  expect(ingredientItemContainer).toBeNull(); 
 });
