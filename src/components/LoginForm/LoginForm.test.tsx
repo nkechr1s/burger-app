ab02400b0686a4447f8f310a9e8cce325c1a9c25
Nikos Kechris <nkechris22@gmail.com>
2024-07-07 03:46:58 +0300

    Mock login func

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,7 +1,15 @@
 import { render, screen,fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import LoginForm from "./LoginForm";
-import { AuthProvider } from "../../context";
+import { AuthProvider, useAuth } from "../../context";
+import { AuthContextType } from "../../types";
+
+jest.mock("../../context", () => ({
+    ...jest.requireActual("../../context"),
+    useAuth: jest.fn(),
+  }));
+  
+  const mockLogin = jest.fn(); // Mock function for auth.login
 
 const LoginFormWithProvider = () => {
   return (
@@ -13,6 +21,16 @@ const LoginFormWithProvider = () => {
   );
 };
 
+beforeEach(() => {
+    mockLogin.mockClear();
+
+    // Mock useAuth to return mockLogin function
+   (useAuth as jest.MockedFunction<typeof useAuth>).mockReturnValue({
+       login: mockLogin,
+       isAuthenticated: false, // Mock other necessary context values here
+       logout: jest.fn(),
+   } as unknown as AuthContextType);
+});
 test("renders the Login Form component", () => {
   render(<LoginFormWithProvider />);
   const loginForm = screen.getByTestId("login-form");
@@ -39,3 +57,22 @@ test("typing into email input updates state", () => {
   
     expect(passwordInput).toHaveValue("test123");
   });
+
+test("submitting the form calls handleSubmitEvent function", () => {
+  render(<LoginFormWithProvider />);
+
+  const emailInput = screen.getByLabelText("Email:");
+  const passwordInput = screen.getByLabelText("Password:");
+  const submitButton = screen.getByText("Login");
+
+  fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+  fireEvent.change(passwordInput, { target: { value: "test123" } });
+
+  fireEvent.click(submitButton);
+
+  // Assert that login function is called
+  expect(mockLogin).toHaveBeenCalledWith({
+    name: "test@example.com",
+    password: "test123",
+  });
+});
